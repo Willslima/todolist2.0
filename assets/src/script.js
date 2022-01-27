eed5d4968f6d135a0b4638a5d2ed18fbbc712739
Willslima <williansantos38@gmail.com>
2022-01-27 10:35:00 -0300

    feita a lógica de inserção do input e texto no html via js

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+function InsertToDo(){
+    let getInput = document.querySelector('input').value
+    if (!getInput) {
+        console.log('insira um text')
+        return 
+    }
+    console.log(getInput)
+
+    let getUl = document.querySelector('ul')
+    let createLi = document.createElement('li')
+    let createP = document.createElement('p')
+    let createCheckbox = document.createElement('input')
+
+    createCheckbox.type = 'checkbox'
+    createLi.append(createCheckbox)
+    createP.textContent = getInput
+    getUl.append(createLi)
+    createLi.append(createP)
+}
